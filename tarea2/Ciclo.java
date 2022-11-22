4c0d712294972b759629446b926ac3f901c3e514
dsarotti <sr.sarotti@gmail.com>
2022-11-22 18:07:21 +0100

    constructor

--- tarea2/Ciclo.java
+++ tarea2/Ciclo.java
@@ -4,20 +4,20 @@ public class Ciclo {
     public static final int TIPO_MODULAR = 1;
     public static final int TIPO_ORDINARIO = 2;
     public static final int TIPO_DUAL = 3;
-
+    
+    private final String nombre;
     private final int tipo;
     private final String codigo;
-    private final String nombre;
     private ArrayList<Modulo> modulos = new ArrayList<Modulo>();
 
-    public Ciclo(String nombre, int tipo){
+    public Ciclo(String nombre, int tipo, String codigo){
         this.nombre = nombre;
         this.tipo=tipo;
-
+        this.codigo = codigo;
     }
 
-    public Ciclo(String nombre, int tipo, ArrayList<Modulo> modulos){
-        this(nombre,tipo);
+    public Ciclo(String nombre, int tipo,String codigo, ArrayList<Modulo> modulos){
+        this(nombre,tipo, codigo );
         this.modulos = modulos;
     }
     
