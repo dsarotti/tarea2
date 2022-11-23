3aaba74fe78910d0d7d16e2e18810635a7188dec
dsarotti <sr.sarotti@gmail.com>
2022-11-23 20:53:23 +0100

    multiples modificaciones

--- src/Alumno.java
+++ src/Alumno.java
@@ -1,10 +1,11 @@
 import java.util.Date;
+
 /*
  * Almacena información sobre los alumnos
  */
-public class Alumno{
+public class Alumno {
     private Matricula matricula;
-    private String DNI;
+    private DNI dni;
     private String nombre;
     private String apellido;
     private Date fechaNac;
@@ -13,5 +14,18 @@ public class Alumno{
     private String direccion;
     private boolean esDelegado = false;
 
-    
+    public Alumno(Matricula matricula, DNI dni, String nombre, String apellido, Date fechaNac, int telefono,
+            String mail, String direccion, boolean esDelegado) throws IllegalArgumentException {
+
+        this.dni = dni;
+        this.matricula = matricula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaNac = fechaNac;
+        this.telefono = telefono;
+        this.mail = mail;
+        this.direccion = direccion;
+        this.esDelegado = esDelegado;
+    }
+
 }
