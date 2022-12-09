2efc92d2e63098654212eeda7394af94cf44af5b
dsarotti <sr.sarotti@gmail.com>
2022-12-09 04:14:55 +0100

    javadocs

--- tarea2/ciclos/Aula.java
+++ tarea2/ciclos/Aula.java
@@ -8,10 +8,20 @@ public class Aula {
     private int capacidad;
     private ArrayList<String> material;
 
+    /**
+     * 
+     */
     public Aula() {
 
     }
 
+    /**
+     * 
+     * @param nombrePlanta
+     * @param numero
+     * @param capacidad
+     * @param material
+     */
     public Aula(String nombrePlanta, int numero, int capacidad, ArrayList<String> material) {
         this.nombrePlanta = nombrePlanta;
         this.numero = numero;
@@ -19,38 +29,68 @@ public class Aula {
         this.material = material;
     }
 
+    /**
+     * 
+     * @return nombrePlanta
+     */
+
     public String getNombrePlanta() {
         return nombrePlanta;
     }
 
+    /**
+     * 
+     * @param nombrePlanta
+     */
     public void setNombrePlanta(String nombrePlanta) {
         this.nombrePlanta = nombrePlanta;
     }
 
+    /**
+     * @return int
+     */
     public int getNumero() {
         return numero;
     }
 
+    /**
+     * @param numero
+     */
     public void setNumero(int numero) {
         this.numero = numero;
     }
 
+    /**
+     * @return int
+     */
     public int getCapacidad() {
         return capacidad;
     }
 
+    /**
+     * @param capacidad
+     */
     public void setCapacidad(int capacidad) {
         this.capacidad = capacidad;
     }
 
+    /**
+     * @return ArrayList<String>
+     */
     public ArrayList<String> getMaterial() {
         return material;
     }
 
+    /**
+     * @param material
+     */
     public void setMaterial(ArrayList<String> material) {
         this.material = material;
     }
 
+    /**
+     * @return String
+     */
     @Override
     public String toString() {
         return "Aula [nombrePlanta=" + nombrePlanta + ", numero=" + numero + ", capacidad=" + capacidad + ", material="
--- tarea2/ciclos/Curso.java
+++ tarea2/ciclos/Curso.java
@@ -21,40 +21,66 @@ public class Curso {
         this.modulosProfesores = modulosProfesores;
         if (esAlumno(delegado, alumnos)) {
             this.delegado = delegado;
+            this.delegado.setDelegado();
         } else
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("El delegado debe ser alumno del curso.");
         if (esProfesor(tutor, modulosProfesores)) {
             this.tutor = tutor;
         } else
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("El tutor debe ser profesor de alguno de los modulos del curso.");
     }
 
+    /**
+     * @return Alumno
+     */
     public Alumno getDelegado() {
         return delegado;
     }
 
+    /**
+     * @param delegado
+     * @throws IllegalArgumentException
+     */
     public void setDelegado(Alumno delegado) throws IllegalArgumentException {
         if (esAlumno(delegado, alumnos)) {
             this.delegado = delegado;
+            this.delegado.setDelegado();
         } else
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("El delegado debe ser alumno del curso.");
     }
 
-    public boolean esAlumno(Alumno alumno, ArrayList<Alumno> alumnos) {
+    /**
+     * @param alumno
+     * @param alumnos
+     * @return boolean
+     */
+    public static boolean esAlumno(Alumno alumno, ArrayList<Alumno> alumnos) {
         return alumnos.contains(alumno);
     }
 
+    /**
+     * @return Profesor
+     */
     public Profesor getTutor() {
         return tutor;
     }
 
+    /**
+     * @param tutor
+     * @throws IllegalArgumentException
+     */
     public void setTutor(Profesor tutor) throws IllegalArgumentException {
         if (esProfesor(tutor, modulosProfesores)) {
             this.tutor = tutor;
         } else
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("El tutor debe ser profesor de alguno de los modulos del curso.");
     }
 
+    /**
+     * @param profesor
+     * @param modulosProfesores
+     * @return boolean
+     */
     public boolean esProfesor(Profesor profesor, ArrayList<ModuloProfesor> modulosProfesores) {
         boolean t = false;
         for (ModuloProfesor moduloProfesor : modulosProfesores) {
@@ -66,22 +92,37 @@ public class Curso {
         return t;
     }
 
+    /**
+     * @return ArrayList<Alumno>
+     */
     public ArrayList<Alumno> getAlumnos() {
         return alumnos;
     }
 
+    /**
+     * @param alumnos
+     */
     public void setAlumnos(ArrayList<Alumno> alumnos) {
         this.alumnos = alumnos;
     }
 
+    /**
+     * @return ArrayList<ModuloProfesor>
+     */
     public ArrayList<ModuloProfesor> getModulosProfesores() {
         return modulosProfesores;
     }
 
+    /**
+     * @param modulosProfesores
+     */
     public void setModulosProfesores(ArrayList<ModuloProfesor> modulosProfesores) {
         this.modulosProfesores = modulosProfesores;
     }
 
+    /**
+     * @return String
+     */
     @Override
     public String toString() {
         return "Curso [alumnos=" + alumnos + ", modulosProfesores=" + modulosProfesores + ", delegado=" + delegado
